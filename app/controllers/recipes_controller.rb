00ea05e0cc2df81d061c9c04a1f34096540d0749
louis carment <louiscarment@gmail.com>
2024-06-04 18:16:56 +0200

    mise en place de l'index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,12 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
 
   def new
     @recipe = Recipe.new
