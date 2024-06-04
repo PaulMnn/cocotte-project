b7ef4463277365845fff08e790d5ba7f23b3ff57
snith nhotprasa <lesnith@gmail.com>
2024-06-04 16:50:10 +0200

    formulaire front non cleared

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,11 @@ class Recipe < ApplicationRecord
   has_many :ebooks, through: :recipages
   validates :title, presence: true
   validates :meal, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_ingredient_meal_instruction,
+  against: [ :title, :ingredient, :meal, :instruction ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
