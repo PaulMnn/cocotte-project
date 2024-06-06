a685feb0d687cb8cedd79cc28f79d5d424f6a5da
louis carment <louiscarment@gmail.com>
2024-06-06 14:59:22 +0200

    filter bar and search bar

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,8 +4,19 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  # def index
+  #   @recipes = Recipe.all
+  #   if params[:query].present?
+  #     @recipes = @recipes.where(title: params[:query])
+  #   end
+  # end
+
   def index
     @recipes = Recipe.all
+    if params[:query].present?
+      sql_subquery = "title ILIKE :query OR instruction ILIKE :query"
+      @recipes = @recipes.where(sql_subquery, query: "%#{params[:query]}%")
+    end
   end
 
   def new
