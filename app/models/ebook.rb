ac395f8a4b3df86d182314d20f13558cb1a8be50
Paul Manein <manein.paul@gmail.com>
2024-06-07 13:21:40 +0200

    view show ebook details

--- app/models/ebook.rb
+++ app/models/ebook.rb
@@ -2,8 +2,20 @@ class Ebook < ApplicationRecord
   belongs_to :user
   has_many :recipages
   has_many :recipes, through: :recipages
-  has_many :recipages
   validates :theme, presence: true
   validates :ebook_title, presence: true
   has_one_attached :photo
+
+  def dishes_count
+    recipes.where(meal: 'Plat').count
+  end
+
+  def starters_count
+    recipes.where(meal: 'Entrée').count
+  end
+
+  def desserts_count
+    recipes.where(meal: 'Dessert').count
+  end
+
 end
