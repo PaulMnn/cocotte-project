64de2e5b589b76e4c3779f29cd7a06df4d145e41
snith nhotprasa <lesnith@gmail.com>
2024-06-10 20:10:34 +0200

    backup et js

---
+++ app/javascript/controllers/deselectable_radio_buttons_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="deselectable-radio-buttons"
+export default class extends Controller {
+  connect() {
+    this.element.querySelectorAll('input[type="radio"]').forEach(radio => {
+      radio.addEventListener('click', this.toggleCheck);
+    })
+  }
+
+  toggleCheck(event) {
+    const radio = event.currentTarget
+    if (radio.checked) {
+      radio.checked =!radio.checked;
+    }
+  }
+}
